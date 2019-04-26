253d2c6f8f47986c33b91ac366e814087217e5ee
Fabian Geiselhart <me@f4814n.de>
2019-04-26 14:48:54 +0200

    [Views] Make watcher interface usable

--- lamon/forms.py
+++ lamon/forms.py
@@ -1,18 +1,70 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField
-from wtforms.validators import DataRequired
-
-from .models import Watcher
+from wtforms import (HiddenField, TextField, FieldList, IntegerField, FormField,
+                     SubmitField, SelectField)
+from wtforms.validators import DataRequired, Optional
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 
 class WatcherControlForm(FlaskForm):
-    watcherID = SelectField('Watcher', validators=[DataRequired()], coerce=int)
-    action = SelectField('Action', validators=[DataRequired()],
-                         choices=[
-        ('START', 'Start'),
-        ('STOP', 'Stop'),
-        ('RELOAD', 'Reload')
+    """ Form used to control a watcher. """
+    reload = SubmitField('reload')
+    start = SubmitField('start')
+    stop = SubmitField('stop')
+    watcher_id = HiddenField('watcher_id', validators=[DataRequired()])
+
+    @classmethod
+    def for_watcher(cls, watcher_id):
+        """ Prepare form for a specific watcher """
+        cls.watcher_id = HiddenField('watcher_id',
+                                     validators=[DataRequired()],
+                                     default=watcher_id)
+
+        return cls
+
+
+class WatcherCreateForm(FlaskForm):
+    """ Form used to select which thread_class the newly created watcher will
+    use
+    """
+    watcher_class = SelectField('Watcher Class', validators=[DataRequired()],
+                                choices=[
+        ('lamon.watcher.plugin.source_engine.SourceEngineWatcher',
+         'Source Engine Watcher'),
+        ('lamon.watcher.plugin.quake3.Quake3Watcher',
+         'Quake3 Watcher'),
+        ('tests.test_watcher.fake.FakeWatcher',
+         'Fake Watcher')
     ])
 
-    def update_choices(self):
-        self.watcherID.choices = [(g.id, str(g)) for g in Watcher.query.all()]
+
+class WatcherEditForm(FlaskForm):
+    """ Form used to edit a watcher instance """
+    game = QuerySelectField(allow_blank=True)
+
+    @classmethod
+    def load_config(cls, watcher_class, watcher_model):
+        """ Prepare form for a specific watcher """
+        cls.watcher = watcher_class
+        cls.game = QuerySelectField(
+            allow_blank=True, default=watcher_model.game)
+
+        for key, opts in watcher_class.config_keys.items():
+            value = None
+
+            for i in watcher_model.config:
+                if i.key == key:
+                    value = i.value
+
+            if watcher_class.config_keys[key]['type'] is int:
+                field = IntegerField
+            else:
+                field = TextField
+
+            if watcher_class.config_keys[key]['required']:
+                validators = [DataRequired()]
+            else:
+                validators = [Optional()]
+
+            setattr(cls, key, field(key, default=value, validators=validators))
+
+        return cls
--- lamon/views/watcher.py
+++ lamon/views/watcher.py
@@ -1,50 +1,120 @@
-from flask import Blueprint, render_template, request, current_app, flash
-from flask_user import roles_required
+from flask import Blueprint, render_template, request, current_app, flash, redirect, url_for
+from flask_user import roles_required, current_user
+from sqlalchemy.orm.exc import NoResultFound
 
-from ..forms import WatcherControlForm
-from ..models import Watcher
+from .. import db
+from ..forms import WatcherControlForm, WatcherCreateForm, WatcherEditForm
+from ..models import Watcher, Game, WatcherConfig
+from ..watcher import load_watcher_class
 
 watcher_blueprint = Blueprint(
     'watchers', __name__, template_folder='templates')
 
 
 @watcher_blueprint.route('/')
-def index():
-    return render_template('watcher/index.html')
-
-
-@watcher_blueprint.route('/stats/')
-@watcher_blueprint.route('/stats/<int:watcher_id>')
-def stats(watcher_id=None):
-    if watcher_id is None:
-        watchers = Watcher.query.all()
-        return render_template('watcher/stats.html', watchers=watchers)
-    else:
+@watcher_blueprint.route('/<int:watcher_id>')
+def index(watcher_id=None):
+    if watcher_id:
         watcher = Watcher.query.filter(Watcher.id == watcher_id).one()
-        return render_template('watcher/stats_one.html', watcher=watcher)
+        form = WatcherControlForm.for_watcher(watcher.id)()
+
+        return render_template('watcher/index_one.html',
+                               watcher=watcher, form=form)
+    else:
+        watchers = []
+        show_controls = current_user.is_authenticated and current_user.has_role(
+            'admin')
+
+        for watcher in db.session.query(Watcher).all():
+            if show_controls:
+                form = WatcherControlForm.for_watcher(watcher.id)()
+            else:
+                form = None
+
+            watchers.append((watcher, form))
+
+        return render_template('watcher/index.html', watchers=watchers,
+                               show_controls=show_controls)
 
 
-@watcher_blueprint.route('/control', methods=['GET', 'POST'])
+@watcher_blueprint.route('/<int:watcher_id>/edit', methods=['GET', 'POST'])
 @roles_required(['admin'])
-def control():
-    app = current_app
+def edit(watcher_id):
+    watcher_model = Watcher.query.filter(Watcher.id == watcher_id).one()
+    watcher_class = load_watcher_class(watcher_model.threadClass)
 
-    form = WatcherControlForm()
-    form.update_choices()
+    form = WatcherEditForm.load_config(watcher_class, watcher_model)()
+    form.game.query = Game.query
 
     if request.method == 'POST':
         if form.validate_on_submit():
-            try:
-                if form.data['action'] == 'START':
-                    app.watcher_manager.start(id=form.data['watcherID'])
-                    flash('Watcher started successfully', 'success')
-                elif form.data['action'] == 'STOP':
-                    app.watcher_manager.stop(id=form.data['watcherID'])
-                    flash('Watcher stopped successfully', 'success')
-                else:  # RELOAD
-                    app.watcher_manager.reload(id=form.data['watcherID'])
-                    flash('Watcher reloaded successfully', 'success')
-            except ValueError as e:
-                flash(e, 'warning')
+            watcher_model.game = form.data['game']
 
-    return render_template('watcher/control.html', form=form)
+            # XXX This is probably ugly
+            for key, opts in watcher_class.config_keys.items():
+                try:
+                    watcher_config = WatcherConfig.query.\
+                        filter(WatcherConfig.watcher == watcher_model).\
+                        filter(WatcherConfig.key == key).\
+                        update({'value': form.data[key]})
+                except NoResultFound:
+                    watcher_config = WatcherConfig(watcher=watcher_model,
+                                                   key=key,
+                                                   value=form.data[key])
+                    db.session.add(watcher_config)
+
+            db.session.commit()
+
+            return redirect(url_for('watchers.index', watcher_id=watcher_id))
+        else:
+            flash(form.errors, 'warning')
+
+    return render_template('watcher/edit.html', watcher=watcher_model, form=form)
+
+
+@watcher_blueprint.route('/<int:watcher_id>/stats')
+def stats(watcher_id):
+    watcher = Watcher.query.filter(Watcher.id == watcher_id).one()
+    return render_template('watcher/stats.html', watcher=watcher)
+
+
+@watcher_blueprint.route('/control', methods=['POST'])
+@roles_required(['admin'])
+def control():
+    app = current_app
+    form = WatcherControlForm()
+
+    if form.validate_on_submit():
+        try:
+            if form.data['start']:
+                app.watcher_manager.start(id=form.data['watcher_id'])
+                flash('Started watcher', 'success')
+            elif form.data['stop']:
+                app.watcher_manager.stop(id=form.data['watcher_id'])
+                flash('Stopped watcher', 'success')
+            elif form.data['reload']:
+                app.watcher_manager.reload(id=form.data['watcher_id'])
+                flash('Reloaded watcher', 'success')
+        except ValueError as e:
+            flash(str(e), 'warning')
+    else:
+        flash(form.errors, 'warning')
+
+    return redirect(request.referrer or url_for('watchers.index'))
+
+
+@watcher_blueprint.route('/create', methods=['GET', 'POST'])
+@roles_required(['admin'])
+def create():
+    app = current_app
+
+    form = WatcherCreateForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            watcher = Watcher(threadClass=form.data['watcher_class'])
+            db.session.add(watcher)
+            db.session.commit()
+            return redirect(url_for('watchers.edit', watcher_id=watcher.id))
+
+    return render_template('watcher/create.html', form=form)
