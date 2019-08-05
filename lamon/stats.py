af8efb68788c2806206b2f0f5f26e4cd4a9cfa6a
Fabian Geiselhart <me@f4814n.de>
2019-08-05 11:34:19 +0000

    (WIP) User score visualization

--- lamon/stats.py
+++ lamon/stats.py
@@ -2,12 +2,16 @@
 from . import db
 from .models import User, Event, EventType
 
+from datetime import datetime
+
+
 def currently_playing(watcher_id=None, game_id=None):
     events = Event.query.\
         filter(Event.type == EventType.USER_JOIN, Event.type == EventType.USER_LEAVE).\
         order_by(Event.time.asc()).all()
 
     return -2
+
 
 def number_of_players(watcher_id=None, game_id=None):
     query = User.query
@@ -20,7 +24,33 @@ def number_of_players(watcher_id=None, game_id=None):
 
     return len(list(query.all()))
 
-def score(game_id=None, user_id=None, timeline=False):
+
+def score(timeline=False, **kwargs):
+    if timeline:
+        return _score_timeline(**kwargs)
+    return _score(**kwargs)
+
+
+def _score_timeline(game_id=None, user_id=None):
+    if user_id is None:
+        raise ValueError('user_id is required')
+
+    events = Event.query.\
+        filter(Event.gameID == game_id).\
+        filter(Event.userID == user_id).\
+        filter(Event.type == EventType.USER_SCORE).all()
+
+    x = []
+    y = []
+
+    for event in events:
+        x.append(event.time.strftime('%Y-%m-%d %H:%M:%S'))
+        y.append(float(event.info))
+
+    return x, y
+
+
+def _score(game_id=None, user_id=None):
     return 3
 
 
